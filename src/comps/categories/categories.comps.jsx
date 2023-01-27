a45ac6e2fa3d445eb2d94d56c06dc22111262833
Mitchell Stowman <mitchellstowman@gmail.com>
2023-01-27 11:30:43 -0500

    Created Categories Structure - Styled using SCSS - Using 'categories' json for info

---
+++ src/comps/categories/categories.comps.jsx
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+
+import './categories.styles.scss';
+
+
+class Categories extends Component {
+    render() {
+        return(
+            <div>Test</div>
+        )
+    }
+}
+
+export default Categories;

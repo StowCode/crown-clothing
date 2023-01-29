b0e48eeb5da3547533052fa0e1fd7a7367db5991
Mitchell Stowman <mitchellstowman@gmail.com>
2023-01-28 19:31:22 -0500

    Added 'Category-Item' component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Categories from './comps/categories/categories.comps';
+import CategoryItem from './comps/category-item/category-item.component';
 
 const App = () => {
 
@@ -33,25 +33,13 @@ const App = () => {
 
   return (
     <div>
-      <Categories />
       <div className='categories-container'>
 
-        {categories.map(({title, id, imageUrl}) => (
-          
-          <div className='category-container' key={id}>
-            <div 
-              className='background-image' 
-                style={{
-                backgroundImage: `url(${imageUrl})`
-                }
-              } 
-            />
-
-              <div className='category-body-container'>
-                <h2>{title}</h2>
-                <p>Shop Now</p>
-              </div>
-          </div>
+        {categories.map((category) => (
+          <CategoryItem 
+            key={category.id} 
+            category={category}
+          />
 
         ))}
       </div>

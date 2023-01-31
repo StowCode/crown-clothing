f5d910a962bc33c6601fa1bde7dbfe3cc88fd33e
Mitchell Stowman <mitchellstowman@gmail.com>
2023-01-31 18:49:12 -0500

    Added Google Auth >> - Configured Firebase >> - Created contact and sign-in comps >> - Created database within firebase

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
 import SignIn from './routes/sign-in/sign-in.component';
+import Contact from './routes/contact/contact.component';
 
 
 const Shop = () => {
@@ -19,6 +20,7 @@ const App = () => {
         <Route path='/' element={<Navigation />}>
           <Route index='true' element={<Home />}/>
           <Route path='/shop' element={<Shop />}/>
+          <Route path='/contact' element={<Contact />}/>
           <Route path='/sign-in' element={<SignIn />}/>
         </Route>
         

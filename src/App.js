caca51a1c6223d764bcb7dd57fd8e0a426dd4dc7
Mitchell Stowman <mitchellstowman@gmail.com>
2023-01-31 12:24:52 -0500

    Added react-router - set up initial routes - created navigation component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import { Routes, Route } from 'react-router-dom'
 
 import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
+
+
+const Shop = () => {
+  return(
+    <h1>Shop Test</h1>
+  )
+}
 
 const App = () => {
 
   return (
     <div>
       <Routes>
-        <Route 
-          path='/' 
-          element={<Home />}
-        />
+        <Route path='/' element={<Navigation />}>
+          <Route index='true' element={<Home />}/>
+          <Route path='/shop' element={<Shop />}/>
+        </Route>
+        
       </Routes>
     </div>
   );
